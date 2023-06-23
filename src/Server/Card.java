47fd8a86688473a9c34ce722b7e8a52b0bb57e0d
patraisz <131159559+patraisz@users.noreply.github.com>
2023-06-23 11:29:52 +0200

    Message

--- src/Server/Card.java
+++ src/Server/Card.java
@@ -2,4 +2,13 @@ package Server;
 
 public class Card {
 
-}
+
+    private static String[] currentPosition = {
+                "H7", "H8", "H9", "HX", "HB", "HD", "HK", "HA",
+                "C7", "C8", "C9", "CX", "CB", "CD", "CK", "CA",
+                "P7", "P8", "P9", "PX", "PB", "PD", "PK", "PA",
+                "K7", "K8", "K9", "KX", "KB", "KD", "KK", "KA"
+        };
+    }
+
+
--- src/Server/Player.java
+++ src/Server/Player.java
@@ -6,16 +6,17 @@ public class Player {
     String playerIP;
     boolean isConnected;
     String playerName;
+    Player platzhalter = new Player ("1", true, "Platzhalter", null);
     // yourTurn -> Variable notwendig?
     // maxSpieler -> Server.java
 
 
-    public Player(String playerIP, boolean isConnected, String playerName){
+    public Player(String playerIP, boolean isConnected, String playerName, Player nextPlayer){
         playerIP = "1.2.3";
         isConnected = true;
         playerName = "Wait";
+        nextPlayer = platzhalter;
     }
-
 
 
     // Liste Reihenfolge
