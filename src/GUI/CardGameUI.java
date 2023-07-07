354881bce814174d33421c1b06c266caf7d5846a
SmolBlackHole <cgoerlac@gmail.com>
2023-07-07 05:20:51 +0200

    GUI

--- src/GUI/CardGameUI.java
+++ src/GUI/CardGameUI.java
@@ -2,14 +2,43 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.*;
 
 public class CardGameUI {
+
+    // Breite und Höhe einer Karte
+    private static final int CARD_WIDTH = (int) (73 * 1.5);
+    private static final int CARD_HEIGHT = (int) (97 * 1.5);
+
+    //  Offset beim Karten verschieben (beim Hovern)
+    private static final int HOVER_OFFSET = -20 * 2;
+
+    // Anfangsposition der Karten
+    private static final int INITIAL_X = 1100;
+    private static final int INITIAL_Y = 650;
+
+    //  Faktor, um die Karte beim Hovern zu vergrößern
+    private static final double HOVER_SCALE_FACTOR = 1.5;
+
+    // Pfad zum Hintergrundbild
+    private static final String BACKGROUND_IMAGE_PATH = "src/GUI/Assets/Table.jpg";
+
+    // Liste zur Speicherung der Karten-Labels
+    private List<JLabel> cardLabels;
+
+    // Array zur Speicherung der ursprünglichen Positionen der Karten
+    private Point[] originalCardLocations;
+
+    // Label zur Darstellung der vergrößerten Karte in der Mitte
+    private JLabel enlargedCardLabel;
+
+    // Breite und Höhe des Fensters
+    private static final int WindowWidth = 1440;
+    private static final int WindowHeight = 900;
 
     public CardGameUI() {
         EventQueue.invokeLater(new Runnable() {
@@ -22,282 +51,224 @@ public class CardGameUI {
                     ex.printStackTrace();
                 }
 
+                // Erzeugen des Kartendecks
+                List<String> cardDeck = Card.getCardDeck();
 
-                // gescheiterter Versuch den Server irgendwie auszutricksen
-                Turn.createPlayer("127.0.0.1", "Player 1", null);
-                Turn.createPlayer("127.0.0.1", "Player 2", null);
-                Turn.createPlayer("127.0.0.1", "Player 3", null);
-                Turn.connectFirstAndLast();
-
-                List<Turn.Hand> players = new ArrayList<>(3);
-                Turn.Player temp = Turn.firstPlayer;
-                do {
-                    players.add(new Turn.Hand(temp));
-                    temp = temp.nextPlayer;
-                } while (temp != Turn.firstPlayer);
-
+                // Erzeugen des Hauptfensters
                 JFrame frame = new JFrame("Card Game");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(new GamePane(players));
-                frame.pack();
+                frame.setSize(WindowWidth, WindowHeight);
+
+                // Haupt-Panel mit Hintergrundbild
+                JPanel mainPanel = new JPanel(null) {
+                    @Override
+                    protected void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+                        ImageIcon backgroundIcon = new ImageIcon(BACKGROUND_IMAGE_PATH);
+                        Image background = backgroundIcon.getImage();
+                        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+                    }
+                };
+                frame.setContentPane(mainPanel);
+
+                // Erstellen der Karten-Labels und Hinzufügen zum Hauptpanel
+                cardLabels = new ArrayList<>();
+                enlargedCardLabel = createEnlargedCardLabel();
+
+                int x = INITIAL_X;
+                int y = INITIAL_Y;
+
+                for (int i = cardDeck.size() - 1; i >= 0; i--) {
+                    String card = cardDeck.get(i);
+                    JLabel cardLabel = createCardLabel(card);
+                    cardLabel.setBounds(x, y, CARD_WIDTH, CARD_HEIGHT);
+                    mainPanel.add(cardLabel);
+                    cardLabels.add(cardLabel);
+
+                    x -= 30;
+                }
+
+                // Speichern der ursprünglichen Kartenpositionen
+                originalCardLocations = new Point[cardLabels.size()];
+                for (int i = 0; i < cardLabels.size(); i++) {
+                    originalCardLocations[i] = cardLabels.get(i).getLocation();
+                }
+
+                // Hinzufügen des Labels für die vergrößerte Karte
+                mainPanel.add(enlargedCardLabel);
+
+                // Hinzufügen der MouseListener zu den Karten-Labels
+                addMouseListeners(frame);
+
                 frame.setLocationRelativeTo(null);
                 frame.setVisible(true);
             }
         });
     }
 
-    public static void main(String[] args) {
-        new CardGameUI();
+    // Methode zur Erstellung eines Karten-Labels
+    private JLabel createCardLabel(String card) {
+        JLabel cardLabel = new JLabel();
+        ImageIcon imageIcon = new ImageIcon("src/GUI/Assets/" + card + ".png");
+        Image image = imageIcon.getImage();
+        Image scaledImage = image.getScaledInstance(CARD_WIDTH, CARD_HEIGHT, Image.SCALE_SMOOTH);
+        ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
+        cardLabel.setIcon(scaledImageIcon);
+        return cardLabel;
     }
 
-    public static class GamePane extends JPanel {
+    // Methode zur Erstellung des Labels für die vergrößerte Karte
+    private JLabel createEnlargedCardLabel() {
+        JLabel enlargedCardLabel = new JLabel();
+        enlargedCardLabel.setVisible(false);
+        return enlargedCardLabel;
+    }
 
-        private final List<Turn.Hand> players;
+    // Methode zum Hinzufügen der MouseListener zu den Karten-Labels
+    private void addMouseListeners(JFrame frame) {
+        for (int i = 0; i < cardLabels.size(); i++) {
+            int index = i;
+            JLabel cardLabel = cardLabels.get(i);
+            cardLabel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    moveCard(cardLabel, HOVER_OFFSET);
+                    setComponentZOrder(frame, cardLabel, index + cardLabels.size());
+                    showEnlargedCard(cardLabel.getIcon());
+                }
 
-        private final Map<Turn.Card, Rectangle> mapCards;
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    moveCard(cardLabel, 0);
+                    setComponentZOrder(frame, cardLabel, index);
+                    hideEnlargedCard();
+                }
 
-        private Turn.Card selected;
-
-        public GamePane(List<Turn.Hand> players) {
-            this.players = players;
-            mapCards = new HashMap<>(players.size() * 5);
-
-            addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    if (selected != null) {
-                        Rectangle bounds = mapCards.get(selected);
-                        bounds.y += 20;
-                        repaint();
-                    }
-                    selected = null;
-                    for (Turn.Card card : players.get(0).reveresed()) {
-                        Rectangle bounds = mapCards.get(card);
-                        if (bounds.contains(e.getPoint())) {
-                            selected = card;
-                            bounds.y -= 20;
-                            repaint();
-                            break;
-                        }
+                    if (enlargedCardLabel.isVisible()) {
+                        hideEnlargedCard();
+                    } else {
+                        showEnlargedCard(cardLabel.getIcon());
                     }
                 }
             });
-
-            addComponentListener(new ComponentAdapter() {
+            // Füge MouseMotionListener hinzu, um die Z-Reihenfolge der Karten zu aktualisieren
+            cardLabel.addMouseMotionListener(new MouseAdapter() {
                 @Override
-                public void componentResized(ComponentEvent e) {
-                    invalidate();
+                public void mouseMoved(MouseEvent e) {
+                    if (index == cardLabels.size() - 1) {
+                        // Setze die Z-Reihenfolge der letzten Karte auf die Vorderseite
+                        setComponentZOrder(frame, cardLabel, index + cardLabels.size());
+                    } else if (index == 0) {
+                        // Setze die Z-Reihenfolge der ersten Karte auf die Rückseite
+                        setComponentZOrder(frame, cardLabel, index);
+                    }
                 }
             });
-        }
-
-        @Override
-        public Dimension getPreferredSize() {
-            return new Dimension(400, 400);
-        }
-
-        @Override
-        public void invalidate() {
-            super.invalidate();
-            mapCards.clear();
-            Turn.Hand hand = players.get(0);
-            int cardHeight = (getHeight() - 20) / 3;
-            int cardWidth = (int) (cardHeight * 0.6);
-            int xDelta = cardWidth / 2;
-            int xPos = (int) ((getWidth() / 2) - (cardWidth * (hand.size() / 4.0)));
-            int yPos = (getHeight() - 20) - cardHeight;
-            for (Turn.Card card : hand.cards()) {
-                Rectangle bounds = new Rectangle(xPos, yPos, cardWidth, cardHeight);
-                mapCards.put(card, bounds);
-                xPos += xDelta;
-            }
-        }
-
-        // paint.exe
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g.create();
-            Turn.Hand hand = players.get(0);
-            for (Turn.Card card : hand.cards) {
-                Rectangle bounds = mapCards.get(card);
-                System.out.println(bounds);
-                if (bounds != null) {
-                    g2d.setColor(Color.WHITE);
-                    g2d.fill(bounds);
-                    g2d.setColor(Color.BLACK);
-                    g2d.draw(bounds);
-                    Graphics2D copy = (Graphics2D) g2d.create();
-                    paintCard(copy, card, bounds);
-                    copy.dispose();
-                }
-            }
-            g2d.dispose();
-        }
-
-        protected void paintCard(Graphics2D g2d, Turn.Card card, Rectangle bounds) {
-            g2d.translate(bounds.x + 5, bounds.y + 5);
-            g2d.setClip(0, 0, bounds.width - 5, bounds.height - 5);
-
-            String text = card.getFace().getValue() + card.getSuit().getValue();
-            FontMetrics fm = g2d.getFontMetrics();
-
-            g2d.drawString(text, 0, fm.getAscent());
-        }
-
-
-        // kp was ich hier machen wollte
-        @Override
-        public void addNotify() {
-            super.addNotify();
-            revalidate();
-        }
-
-        @Override
-        public void removeNotify() {
-            super.removeNotify();
-            mapCards.clear();
         }
     }
 
-    // von Patrik geklauter und modifizierter Code
-    public static class Turn {
+    // Methode zum Verschieben einer Karte
+    private void moveCard(JLabel cardLabel, int yOffset) {
+        int cardIndex = cardLabels.indexOf(cardLabel);
+        Point originalLocation = originalCardLocations[cardIndex];
+        Point newLocation = new Point(originalLocation.x, originalLocation.y + yOffset);
+        cardLabel.setLocation(newLocation);
+    }
 
-        static Player firstPlayer;
-        static Player prevPlayer;
-
-        public static void createPlayer(String playerIP, String playerName, Server.Server.ClientHandler playerID) {
-            Player player = new Player(playerIP, playerName, playerID);
-            if (firstPlayer == null) {
-                firstPlayer = player;
-            }
+    // Methode zum Ändern der Z-Reihenfolge einer Komponente
+    private void setComponentZOrder(JFrame frame, Component component, int position) {
+        Container contentPane = frame.getContentPane();
+        if (position < 0 || position >= contentPane.getComponentCount()) {
+            return;
         }
 
-        public static void connectFirstAndLast() {
-            Player tempPlayer = firstPlayer;
-            while (tempPlayer.getNextPlayer() != null) {
-                tempPlayer = tempPlayer.getNextPlayer();
-            }
-            tempPlayer.setNextPlayer(firstPlayer);
-            prevPlayer = tempPlayer;
+        Component[] components = contentPane.getComponents();
+        if (components[position] == component) {
+            return;
         }
 
-        public enum Suit {
-            CLUBS("C"),
-            DIAMONDS("D"),
-            HEARTS("H"),
-            SPADES("S");
+        contentPane.setComponentZOrder(component, position);
+        contentPane.validate();
+    }
 
-            private final String value;
+    // Methode zum Anzeigen der vergrößerten Karte
+    private void showEnlargedCard(Icon cardIcon) {
+        enlargedCardLabel.setIcon(cardIcon);
+        int enlargedWidth = (int) (CARD_WIDTH * HOVER_SCALE_FACTOR);
+        int enlargedHeight = (int) (CARD_HEIGHT * HOVER_SCALE_FACTOR);
+        enlargedCardLabel.setBounds(
+                (WindowWidth - enlargedWidth) / 2,
+                (WindowHeight - enlargedHeight) / 2,
+                enlargedWidth,
+                enlargedHeight
+        );
+        enlargedCardLabel.setVisible(true);
+    }
 
-            Suit(String value) {
-                this.value = value;
+    // Methode zum Ausblenden der vergrößerten Karte
+    private void hideEnlargedCard() {
+        enlargedCardLabel.setVisible(false);
+    }
+
+    // Hauptmethode des Programms, die das GUI startet
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new CardGameUI();
             }
+        });
+    }
 
-            public String getValue() {
-                return value;
-            }
+    // Professionell von Vanessa geklauter Code (Testzwecke)
+    public static class Card {
+        private static final ArrayList<String> cardDeck = new ArrayList<>();
+
+        public static void mixCards() {
+            cardDeck.add("H7");
+            cardDeck.add("H8");
+            cardDeck.add("H9");
+            cardDeck.add("HX");
+            cardDeck.add("HB");
+            cardDeck.add("HD");
+            cardDeck.add("HK");
+            cardDeck.add("HA");
+
+            cardDeck.add("C7");
+            cardDeck.add("C8");
+            cardDeck.add("C9");
+            cardDeck.add("CX");
+            cardDeck.add("CB");
+            cardDeck.add("CD");
+            cardDeck.add("CK");
+            cardDeck.add("CA");
+
+            cardDeck.add("P7");
+            cardDeck.add("P8");
+            cardDeck.add("P9");
+            cardDeck.add("PX");
+            cardDeck.add("PB");
+            cardDeck.add("PD");
+            cardDeck.add("PK");
+            cardDeck.add("PA");
+
+            cardDeck.add("K7");
+            cardDeck.add("K8");
+            cardDeck.add("K9");
+            cardDeck.add("KX");
+            cardDeck.add("KB");
+            cardDeck.add("KD");
+            cardDeck.add("KK");
+            cardDeck.add("KA");
+
+            Collections.shuffle(cardDeck); // Mischen der Karten
         }
 
-        // Temporärer Code, damit hier wenigstens etwas funktioniert
-        public enum Face {
-            ACE("A"),
-            TWO("2"),
-            THREE("3"),
-            FOUR("4"),
-            FIVE("5"),
-            SIX("6"),
-            SEVEN("7"),
-            EIGHT("8"),
-            NINE("9"),
-            TEN("10"),
-            JACK("J"),
-            QUEEN("Q"),
-            KING("K");
-
-            private final String value;
-
-            Face(String value) {
-                this.value = value;
-            }
-
-            public String getValue() {
-                return value;
-            }
-        }
-
-        public static class Player {
-            private final String playerIP;
-            private final String playerName;
-            private final Server.Server.ClientHandler playerID;
-            private Player nextPlayer;
-
-            public Player(String playerIP, String playerName, Server.Server.ClientHandler playerID) {
-                this.playerIP = playerIP;
-                this.playerName = playerName;
-                this.playerID = playerID;
-            }
-
-            public Player getNextPlayer() {
-                return nextPlayer;
-            }
-
-            public void setNextPlayer(Player nextPlayer) {
-                this.nextPlayer = nextPlayer;
-            }
-        }
-
-        public static class Hand {
-
-            private final List<Card> cards;
-
-            public Hand(Player player) {
-                cards = new ArrayList<>(25);
-                // TODO: Logik, um Karten basierend auf Werten von Spieler Objekt zu bekommen
-                // Irndwie sowas in der Richtung: cards = player.getPlayerCards();
-
-                // Broken Code der irgendwie funktioniert und 2 Karten rendert (verstehe nicht, warum das funktioniert)
-                cards.add(new Card(Suit.CLUBS, Face.TWO));
-                cards.add(new Card(Suit.HEARTS, Face.KING));
-                // ...
-            }
-
-            // Useless Code
-            public void add(Card card) {
-                cards.add(card);
-            }
-
-            public int size() {
-                return cards.size();
-            }
-
-            public Iterable<Card> cards() {
-                return cards;
-            }
-
-            public Iterable<Card> reveresed() {
-                List<Card> reversed = new ArrayList<>(cards);
-                Collections.reverse(reversed);
-                return reversed;
-            }
-        }
-
-        public static class Card {
-
-            private final Suit suit;
-            private final Face face;
-
-            public Card(Suit suit, Face face) {
-                this.suit = suit;
-                this.face = face;
-            }
-
-            public Suit getSuit() {
-                return suit;
-            }
-
-            public Face getFace() {
-                return face;
-            }
+        public static ArrayList<String> getCardDeck() {
+            mixCards();
+            return cardDeck;
         }
     }
 }
---
+++ src/GUI/MainUI.java
@@ -0,0 +1,145 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainUI extends JFrame {
+
+    private CardGameUI cardGameUI; // Referenz auf das CardGameUI-Objekt
+
+    public MainUI() {
+        // Fenstereinstellungen
+        setTitle("Card Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLayout(new GridLayout(2, 1));
+
+        // Button für "Host Server"
+        JButton hostServerButton = new JButton("Host Server");
+        hostServerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showHostServerOptions();
+            }
+        });
+
+        // Button für "Spielen"
+        JButton playButton = new JButton("Spielen");
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showLoginDialog();
+            }
+        });
+
+        // Buttons zum Hauptfenster hinzufügen
+        add(hostServerButton);
+        add(playButton);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainUI();
+            }
+        });
+    }
+
+    private void showHostServerOptions() {
+        // Fenster für Host-Server-Optionen
+        JFrame hostServerFrame = new JFrame("Hosting Optionen");
+        hostServerFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        hostServerFrame.setSize(300, 200);
+        hostServerFrame.setLayout(new GridLayout(3, 2));
+
+        // Label und Textfeld für maximale Spieleranzahl
+        JLabel maxPlayersLabel = new JLabel("Max Spieler:");
+        JTextField maxPlayersField = new JTextField();
+        maxPlayersField.setHorizontalAlignment(JTextField.CENTER);
+
+        // Label und Textfeld für Port
+        JLabel portLabel = new JLabel("Port:");
+        JTextField portField = new JTextField("25565");
+        portField.setHorizontalAlignment(JTextField.CENTER);
+        portField.setEditable(false);
+
+        // Button zum Starten des Servers
+        JButton startHostingButton = new JButton("Start Server");
+        startHostingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int maxPlayers = Integer.parseInt(maxPlayersField.getText());
+                int port = Integer.parseInt(portField.getText());
+                startServer(maxPlayers, port);
+                hostServerFrame.dispose();
+                showLoginDialog();
+            }
+        });
+
+        // Komponenten zum Host-Server-Fenster hinzufügen
+        hostServerFrame.add(maxPlayersLabel);
+        hostServerFrame.add(maxPlayersField);
+        hostServerFrame.add(portLabel);
+        hostServerFrame.add(portField);
+        hostServerFrame.add(new JLabel());
+        hostServerFrame.add(startHostingButton);
+
+        hostServerFrame.setVisible(true);
+    }
+
+    private void showLoginDialog() {
+        // Fenster für Login-Anmeldemaske
+        JFrame loginFrame = new JFrame("Login");
+        loginFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        loginFrame.setSize(300, 200);
+        loginFrame.setLayout(new GridLayout(2, 2));
+
+        // Label und Textfeld für Benutzernamen
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+        usernameField.setHorizontalAlignment(JTextField.CENTER);
+
+        // Button zum Beitritt zum Server
+        JButton joinServerButton = new JButton("Server Beitreten");
+        joinServerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                joinServer(username);
+                loginFrame.dispose();
+                showCardGameUI(); // Hier wird das CardGameUI-Fenster geöffnet
+            }
+        });
+
+        // Komponenten zum Login-Fenster hinzufügen
+        loginFrame.add(usernameLabel);
+        loginFrame.add(usernameField);
+        loginFrame.add(new JLabel());
+        loginFrame.add(joinServerButton);
+
+        loginFrame.setVisible(true);
+    }
+
+    private void startServer(int maxPlayers, int port) {
+        // Code zum Starten des Servers mit den angegebenen Parametern
+        System.out.println("Starting server with max players: " + maxPlayers + ", port: " + port);
+    }
+
+    private void joinServer(String username) {
+        // Code zum Beitritt zum Server mit dem angegebenen Benutzernamen
+        System.out.println("Joining server with username: " + username);
+    }
+
+    private void showCardGameUI() {
+        // Überprüfen, ob die Voraussetzungen erfüllt sind
+        if (cardGameUI == null) {
+            // Wenn das CardGameUI-Objekt noch nicht erstellt wurde, erstelle es
+            cardGameUI = new CardGameUI();
+        }
+    }
+}
